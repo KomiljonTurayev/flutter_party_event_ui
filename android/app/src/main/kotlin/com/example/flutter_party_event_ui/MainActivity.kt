61c28a7bb0812582c07d9f9b5ec3e703031fedf4
Komiljon <turayevkomiljon@gmail.com>
2021-12-02 16:12:50 +0500

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/flutter_party_event_ui/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.flutter_party_event_ui
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_party_event_ui/pages/home_pages.dart';
+
+void main() =>
+    runApp(MaterialApp(debugShowCheckedModeBanner: false, home: HomePage()));
